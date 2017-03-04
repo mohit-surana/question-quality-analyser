357bc25006808b8acee4fe9bb6c047702ede1710
doodhwala <mohit.surana95@gmail.com>
2017-03-04 21:49:22 +0530

    Adding OS parsing code

--- Code/pdfparser_ada.py
+++ Code/pdfparser_ada.py
@@ -1,6 +1,5 @@
-''' Invocation: python adaparser.py <input.pdf> <first section in book> <last section in book>
-	Example: pdfparser.py ADA.pdf Introduction Epilogue
-'''
+# Invocation: python pdfparser_ada.py
+
 import subprocess
 import sys
 import re
@@ -17,23 +16,23 @@ CHAPTER_PATTERN = r'(?:^|\n)[ ]*([\d.]+)?[ ]*(%s)[ ]*([\d]+)[ ]*(?:$|\n)'
 split_mode = SECTION_MODE
 def get_chapters(text):
 	return [{'sno' :match[0],
-			'level' : 3 if match[0] == '' 
-				else 1 if '.' not in match[0] 
-				else 2 if match[0].index('.') == match[0].rindex('.') 
-				else 3, 
-			'title' : re.sub(r'([()])', r'\\\1', re.sub('[\s]+', ' ', match[1].strip())), 
+			'level' : 3 if match[0] == ''
+				else 1 if '.' not in match[0]
+				else 2 if match[0].index('.') == match[0].rindex('.')
+				else 3,
+			'title' : re.sub(r'([()])', r'\\\1', re.sub('[\s]+', ' ', match[1].strip())),
 			'pno' : int(match[2])}
-		for match in re.findall(CHAPTER_PATTERN %(r'[^\d]+?'), '\n'.join(text.split('\n')[1 : -1]), re.M|re.DOTALL) if match[1].strip()[0].isalnum() and 'Summary' not in match[1]]	
+		for match in re.findall(CHAPTER_PATTERN %(r'[^\d]+?'), '\n'.join(text.split('\n')[1 : -1]), re.M|re.DOTALL) if match[1].strip()[0].isalnum() and 'Summary' not in match[1]]
 
 pdfname = 'ADA'
 first_chapter = 'Introduction'
 last_chapter = 'Epilogue'
 
 process = subprocess.Popen('pdftotext -table -fixed 0 -clip resources/%s -' %(pdfname + '.pdf'), shell=True, stdout=subprocess.PIPE)
-text, _ = process.communicate() 
+text, _ = process.communicate()
 
 pages = ('\n'.join(line for line in text.split('\n') if line != '')).split('\f')
-for i, page in enumerate(pages): 
+for i, page in enumerate(pages):
 	if 'brief contents' not in page.lower() and ('contents' in page.lower() or 'table of contents' in page.lower()):
 		break
 
@@ -43,10 +42,10 @@ for j, page in enumerate(pages[i:]):
 
 	if re.search(CHAPTER_PATTERN %(last_chapter), page, re.M):
 		chapter_tree += get_chapters(re.split(CHAPTER_PATTERN %(last_chapter), page, re.M)[0])
-		index = { c['title'] : c['pno'] for c in chapter_tree }	
+		index = { c['title'] : c['pno'] for c in chapter_tree }
 
 		while not (re.match('%d' %index[first_chapter], pages[j + i].split('\n')[0].strip()) or re.match('%d' %index[first_chapter], pages[j + i].split('\n')[-1].strip())):
-			j += 1	
+			j += 1
 		pages = map(lambda x: '\n'.join(x.split('\n')[1:-1]), pages[(j + i) : ])
 		break
 
@@ -87,12 +86,12 @@ for i, (cur_topic, next_topic) in enumerate(zip(chapter_tree[:-1], chapter_tree[
 			section_text = re.sub(chapter_tree[i - 1]['title'], '', section_text, flags=re.M | re.DOTALL)
 
 	section_text = re.search(r'(?:%s[ ]*)?%s[\s]*(.*?)(?:%s[ ]*)?%s[\s]*' %(cur_topic['sno'], r'(?:[\s]+)'.join(cur_topic['title'].split()), next_topic['sno'], r'(?:[\s]+)'.join(next_topic['title'].split())), section_text, re.M | re.DOTALL).group(1)
-	
+
 	section_text = re.sub('[\s]+', ' ', re.sub('[^a-z\s.!?\']', ' ', re.sub('-\n', '', re.sub('\n[\s]*', '\n', section_text.lower(), flags=re.M | re.DOTALL), flags=re.M | re.DOTALL), flags=re.M | re.DOTALL), flags=re.M | re.DOTALL)
-	
+
 	with open(str(cur_topic['pno']) + ' ' + cur_topic['title'] + '.txt', 'w') as f:
 		f.write(section_text)
-	
+
 	preprocessed_sections.append(section_text)
 
 	keywords = keywords.union(map(lambda x: x[0], unigram_rake.run(section_text))).union(map(lambda x: x[0], bigram_rake.run(section_text))).union(map(lambda x: x[0], trigram_rake.run(section_text)))
