ede3c8f06210ca07a2f4d039ab0f05037c3d0353
Shiva Deviah <shivakdeviah@gmail.com>
2017-04-05 02:31:59 +0530

    BiRNN with 66% accuracy

--- Code/brnn.py
+++ Code/brnn.py
@@ -4,39 +4,21 @@ import numpy as np
 import pickle
 import pprint
 import sys
+import os
 
-from gensim.models import Word2Vec
-from keras.preprocessing import sequence
 from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score, classification_report, confusion_matrix
 from utils import clean_no_stopwords
 from utils import get_data_for_cognitive_classifiers
 
 sys.setrecursionlimit(2 * 10 ** 7)
 
-NUM_CLASSES = 6
 
-CURSOR_UP_ONE = '\x1b[1A'
-ERASE_LINE = '\x1b[2K'
-
-with open("models/glove.6B.300d.txt", "r", encoding='utf-8') as lines:
-    w2v = {}
-  
-    for row, line in enumerate(lines):
-        try:
-            w = line.split()[0]
-            vec = np.array(list(map(float, line.split()[1:])))
-            w2v[w] = vec
-        except:
-            continue
-        finally:
-            print(CURSOR_UP_ONE + ERASE_LINE + 'Processed {} GloVe vectors'.format(row + 1))
-
-def sent_to_glove(questions):
+def sent_to_glove(questions, w2v):
 	questions_w2glove = []
 	
 	for question in questions:
 		vec = []
-		for word in question[:10]:
+		for word in question:
 			if word in w2v:
 				vec.append(w2v[word])
 			else:
@@ -46,7 +28,8 @@ def sent_to_glove(questions):
 	return np.array(questions_w2glove)
 
 def relu(z):
-	return z * (z > 0)
+	y = z * (z > 0)
+	return np.clip(y, 0, 10)
 
 def relu_prime(z):
 	return (z > 0)
@@ -59,8 +42,8 @@ class RNN:
 		self.direction = direction
 		self.hidden_size 	= hidden_size
 		self.learning_rate = learning_rate
-		self.f = np.tanh
-		self.f_prime = lambda x: 1 - (x ** 2)
+		self.f = relu #np.tanh
+		self.f_prime = relu_prime #lambda x: 1 - (x ** 2)
 
 		self.Wxh = np.random.randn(hidden_size, input_size) * np.sqrt(2.0 / (hidden_size + input_size))
 		self.Whh = np.random.randn(hidden_size, hidden_size) * np.sqrt(2.0 / (hidden_size * 2))
@@ -71,7 +54,7 @@ class RNN:
 		self.mWxh, self.mWhh, self.mWhy = np.zeros_like(self.Wxh), np.zeros_like(self.Whh), np.zeros_like(self.Why)
 		self.mbh, self.mby = np.zeros_like(self.bh), np.zeros_like(self.by) # memory variables for Adagrad
 		
-		#self.dropout_percent = 0.95
+		self.dropout_percent = 0.2
 
 	def forward(self, x, hprev, do_dropout=False):
 		if(self.direction == 'left'):
@@ -85,29 +68,28 @@ class RNN:
 		for t in range(seq_length):
 			xs[t] = x[t].reshape(-1, 1)
 			hs[t] = self.f(np.dot(self.Wxh, xs[t]) + np.dot(self.Whh, hs[t-1]) + self.bh)
-			#if(do_dropout):
-			#	hs[t] *= np.random.binomial(1, self.dropout_percent, size=hs[t-1].shape)
-			#else:
-			#	hs[t] *= self.dropout_percent
+			
+			if(do_dropout):
+				hs[t] *= np.random.binomial(1, 1 - self.dropout_percent, size=hs[t-1].shape)
+			
 			ys[t] = self.f(np.dot(self.Why, hs[t]) + self.by)
 			ps[t] = np.exp(ys[t]) / np.sum(np.exp(ys[t]))
 		return xs, hs, ys, ps
 
 	def backprop(self, xs, hs, ys, ps, targets, dy, do_dropout=False):
 		if(self.direction == 'left'):
-			xs = {len(xs)-1-k : xs[k] for k in xs}
+			xs = {len(xs) - 1 - k : xs[k] for k in xs}
 
 		dWxh, dWhh, dWhy = np.zeros_like(self.Wxh), np.zeros_like(self.Whh), np.zeros_like(self.Why)
 		dbh, dby = np.zeros_like(self.bh), np.zeros_like(self.by)
 		dhnext = np.zeros_like(hs[-1])
 
 		for t in reversed(range(len(xs))):
-			tmp = dy[t] * self.f_prime(ys[t])# * self.dropout_percent
+			tmp = dy[t] * self.f_prime(ys[t])
 			dWhy += np.dot(tmp, hs[t].T)
 			dby += tmp
 			dh = np.dot(self.Why.T, dy[t]) + dhnext
-			dhraw = dh * (1 - hs[t] ** 2)
-			#dhraw = dh * relu_prime(hs[t])
+			dhraw = dh * self.f_prime(hs[t])
 			dbh += dhraw
 			dWxh += np.dot(dhraw, xs[t].T)
 			dWhh += np.dot(dhraw, hs[t-1].T)
@@ -176,7 +158,6 @@ class BiDirectionalRNN:
 				for ind in range(seq_length):
 					this_dy = np.exp(y_pred[ind]) / np.sum(np.exp(y_pred[ind]))
 					t = np.argmax(y)
-					#t = y
 					this_dy[t] -= 1
 					dy.append(this_dy)
 					dby += this_dy
@@ -193,8 +174,8 @@ class BiDirectionalRNN:
 				self.right.update_params(dWxhr, dWhhr, dWhyr, dbhr, dbyr)
 				self.left.update_params(dWxhl, dWhhl, dWhyl, dbhl, dbyl)
 			
-			print("(val acc: {:.2f}%)".format(self.predict(validation_data) * 100))
-			#save_model(self, e+1)
+			self.predict(validation_data)
+
 		save_model(self)
 		print("\nTraining done.")
 	
@@ -208,58 +189,75 @@ class BiDirectionalRNN:
 			targets.append(tr)
 			predictions.append(op)
 		
-		print('Accuracy Score:', accuracy_score(targets, predictions))
-		print('F1 Score:', f1_score(targets, predictions, average="macro"))
-		# print(precision_score(targets, predictions, average="macro"))
-		# print(recall_score(targets, predictions, average="macro"))
-		print(classification_report(targets, predictions))
-		# print(confusion_matrix(targets, predictions))
-		
+		if not test:
+			print('[val acc:      {:.2f}%]'.format(accuracy_score(targets, predictions) * 100))
+			print('[val f1 score: {:.2f}]'.format(f1_score(targets, predictions, average="macro")))
+			'''
+			print(precision_score(targets, predictions, average="macro"))
+			print(recall_score(targets, predictions, average="macro"))
+			print(confusion_matrix(targets, predictions))
+			'''
+		else:
+			print(classification_report(targets, predictions))
+
 		return accuracy_score(targets, predictions)
+		
+		
 
-def load_data(filename, count):
-	i = 0
-	with open(filename, 'r') as f:
-		reader = csv.reader(f)
-		inputs = []
-		outputs = []
-		for row in reader:
-			inputs.append(row[0])
-			outputs.append(int(row[1]))
-			i += 1
-			if i == count:
-				break
-		return inputs, outputs
-
-def w2v(sentence):
-	words = []
-	for word in sentence.split():
-		try:
-			words.append(model[word])
-		except Exception:
-			pass
-
-	return np.array(words)
-
-def save_model(BRNN):
+def save_model(clf):
 	with open('models/brnn_model.pkl', 'wb') as f:
-		dill.dump(BRNN, f)
+		dill.dump(clf, f)
 
 def load_model():
 	with open('models/brnn_model.pkl', 'rb') as f:
-		BRNN = dill.load(f)
-	return BRNN
+		clf = dill.load(f)
+	return clf
 
 if __name__ == "__main__":
+	NUM_CLASSES = 6
+	INPUT_SIZE = 300
+
+	CURSOR_UP_ONE = '\x1b[1A'
+	ERASE_LINE = '\x1b[2K'
+
 	X_data = []
 	Y_data = []
 	
-	X_train, Y_train, X_test, Y_test = get_data_for_cognitive_classifiers(threshold=0.20, what_type='ada', split=0.8, include_keywords=True, keep_dup=False)
-	
+	X_train, Y_train, X_test, Y_test = get_data_for_cognitive_classifiers(threshold=0.15, what_type='ada', split=0.8, include_keywords=True, keep_dup=False)
+
 	X_data = X_train + X_test
 	Y_data = Y_train + Y_test
+
+	vocabulary = {'the'}
+
+	for x in X_train + X_test:
+	    vocabulary = vocabulary.union(set(x))
+
+	filename = 'glove.840B.%dd.txt' %INPUT_SIZE
+
+	if not os.path.exists('models/%s_saved.pkl' %filename.split('.txt')[0]):
+		print()
+		with open('models/' + filename, "r", encoding='utf-8') as lines:
+		    w2v = {}
+		  
+		    for row, line in enumerate(lines):
+		        try:
+		            w = line.split()[0]
+		            if w not in vocabulary:
+		            	continue
+		            vec = np.array(list(map(float, line.split()[1:])))
+		            w2v[w] = vec
+		        except:
+		            continue
+		        finally:
+		            print(CURSOR_UP_ONE + ERASE_LINE + 'Processed {} GloVe vectors'.format(row + 1))
+
+		dill.dump(w2v, open('models/%s_saved.pkl' %filename.split('.txt')[0], 'wb'))
+
+	else:
+		w2v = dill.load(open('models/%s_saved.pkl' %filename.split('.txt')[0], 'rb'))
 	
-	X_data = sequence.pad_sequences(sent_to_glove(X_data), maxlen=10)
+	X_data = sent_to_glove(X_data, w2v)
 	
 	for i in range(len(Y_data)):
 		v = np.zeros(NUM_CLASSES)
@@ -268,21 +266,22 @@ if __name__ == "__main__":
 	
 	Y_data = np.array(Y_data)
 	
-	X_train = np.array(X_data[: int(len(X_data) * 0.70) ])
-	Y_train = np.array(Y_data[: int(len(X_data) * 0.70) ])
+	X_train = np.array(X_data[: int(len(X_data) * 0.75) ])
+	Y_train = np.array(Y_data[: int(len(X_data) * 0.75) ])
 	
-	X_val = np.array(X_data[int(len(X_data) * 0.70) : int(len(X_data) * 0.8)])
-	Y_val = np.array(Y_data[int(len(X_data) * 0.70) : int(len(X_data) * 0.8)])
+	X_val = np.array(X_data[int(len(X_data) * 0.75) : int(len(X_data) * 0.80)])
+	Y_val = np.array(Y_data[int(len(X_data) * 0.75) : int(len(X_data) * 0.80)])
 	
-	X_test = np.array(X_data[int(len(X_data) * 0.8) :])
-	Y_test = np.array(Y_data[int(len(X_data) * 0.8) :])
+	X_test = np.array(X_data[int(len(X_data) * 0.80) :])
+	Y_test = np.array(Y_data[int(len(X_data) * 0.80) :])
+
+	print('Data Loaded/Preprocessed')
 	
-	INPUT_SIZE = 300
-	HIDDEN_SIZE = 64
+	HIDDEN_SIZE = 128
 	OUTPUT_SIZE = NUM_CLASSES
 	
 	EPOCHS = 5
-	LEARNING_RATE = 0.20
+	LEARNING_RATE = 0.010
 	
 	TRAIN = True
 	RETRAIN = False
@@ -293,12 +292,12 @@ if __name__ == "__main__":
 			BRNN = load_model()
 		else:
 			BRNN = BiDirectionalRNN(INPUT_SIZE, HIDDEN_SIZE, OUTPUT_SIZE, learning_rate=LEARNING_RATE)
-		BRNN.train(training_data=(X_train, Y_train), validation_data=(X_val, Y_val), epochs=EPOCHS, do_dropout=True)
+		BRNN.train(training_data=(X_train, Y_train), validation_data=(X_val, Y_val), epochs=EPOCHS, do_dropout=False)
 		save_model(BRNN)
 	else:
 		BRNN = load_model()
-		# BRNN.predict = BiDirectionalRNN(INPUT_SIZE, HIDDEN_SIZE, OUTPUT_SIZE, learning_rate=LEARNING_RATE).predict
 	
+	print()
 	accuracy = BRNN.predict((X_test, Y_test), True)
 	
 	print("Accuracy: {:.2f}%".format(accuracy * 100))
