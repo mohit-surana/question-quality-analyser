3e86f7432850d03d0463bb1e82db73b329575d65
doodhwala <mohit.surana95@gmail.com>
2017-04-18 06:28:48 +0530

    Tabs to spaces and organize imports

--- Code/resources/keyvalidate.py
+++ Code/resources/keyvalidate.py
@@ -1,13 +1,14 @@
-
-import sys
 import copy
-import re
-import requests
-import threading
 import itertools
-import pprint
-from nltk.stem.wordnet import WordNetLemmatizer
+import os
+import re
 import signal
+import sys
+import threading
+
+import requests
+from nltk.stem.wordnet import WordNetLemmatizer
+
 
 def handler(signum, frame):
     print('Signal handler called with signal', signum)
@@ -17,95 +18,96 @@ signal.signal(signal.SIGINT, handler)
 
 lock = threading.Lock()
 
+
 class ValThread(threading.Thread):
-	def __init__(self, tid, keywords):
-		threading.Thread.__init__(self)
-		self.tid = tid
-		self.keywords = keywords
-		self.valid_keywords = []
-	
-	def run(self):
-		try:
-			self.valid_keywords = list(keyword for keyword in self.keywords if is_valid(keyword))
-		except:
-			pass
+    def __init__(self, tid, keywords):
+        threading.Thread.__init__(self)
+        self.tid = tid
+        self.keywords = keywords
+        self.valid_keywords = []
 
-		lock.acquire()
-		with open(path + '/__ValidatedKeywords.txt', 'a') as f:
-			print >> f, '\n'.join([ str((x, 0)) for x in self.valid_keywords])
+    def run(self):
+        try:
+            self.valid_keywords = list(keyword for keyword in self.keywords if is_valid(keyword))
+        except:
+            pass
 
-		lock.release()
+        lock.acquire()
+        with open(path + '/__ValidatedKeywords.txt', 'a') as f:
+            print >> f, '\n'.join([ str((x, 0)) for x in self.valid_keywords])
+
+        lock.release()
+
 
 def is_valid(phrase):
-	print('Validating', phrase)
-	url = 'http://lookup.dbpedia.org/api/search.asmx/KeywordSearch?QueryString=%s'
-	response = requests.get(url % phrase, 
-			headers={'Accept': 'application/json'}).json()
-	
-	if len(response['results']) == 0:
-		return False
+    print('Validating', phrase)
+    url = 'http://lookup.dbpedia.org/api/search.asmx/KeywordSearch?QueryString=%s'
+    response = requests.get(url % phrase, headers={'Accept': 'application/json'}).json()
 
-	for result in response['results']:
-		#if result['description'] != None and re.search('comput(?:ing|er science)|algo(?:rithm|rithmic)?|programming(?: language)?', result['description'].lower(), flags=re.M):
-		if result['description'] != None and re.search(r'operating system(?:s)?|\bos\b', result['description'].lower(), flags=re.M):
-		
-				return True
+    if len(response['results']) == 0:
+        return False
 
-	return False
+    for result in response['results']:
+        #if result['description'] != None and re.search('comput(?:ing|er science)|algo(?:rithm|rithmic)?|programming(?: language)?', result['description'].lower(), flags=re.M):
+        if result['description'] != None and re.search(r'operating system(?:s)?|\bos\b', result['description'].lower(), flags=re.M):
+
+                return True
+
+    return False
 
 if __name__ == '__main__':
-	if len(sys.argv) < 2:
-		sys.exit('Not enough arguments')
+    if len(sys.argv) < 2:
+        sys.exit('Not enough arguments')
 
-	num_threads = 16 if len(sys.argv) == 2 else min(64, max(16, int(sys.argv[2])))
+    num_threads = 16 if len(sys.argv) == 2 else min(64, max(16, int(sys.argv[2])))
 
-	path = sys.argv[1]
+    path = sys.argv[1]
 
-	ngrams = { 'trigrams' : set(), 'bigrams' : set(), 'unigrams' : set() } 
+    ngrams = { 'trigrams' : set(), 'bigrams' : set(), 'unigrams' : set() }
 
-	lemmatizer = WordNetLemmatizer()
-	with open(path + '/__Keywords.txt') as f:
-		for line in f:
-			if not re.match('[a-z]', line):
-				continue
-			words = [w for w, g in itertools.groupby(line.rstrip('\n').split())]
-			words.append(lemmatizer.lemmatize(words.pop()))
-			line = ' '.join(words)
+    lemmatizer = WordNetLemmatizer()
+    with open(path + '/__Keywords.txt') as f:
+        for line in f:
+            if not re.match('[a-z]', line):
+                continue
+            words = [w for w, g in itertools.groupby(line.rstrip('\n').split())]
+            words.append(lemmatizer.lemmatize(words.pop()))
+            line = ' '.join(words)
 
-			if len(words) == 3:
-				ngrams['trigrams'].add(line)
+            if len(words) == 3:
+                ngrams['trigrams'].add(line)
 
-			elif len(words) == 2:
-	 			ngrams['bigrams'].add(line)
+            elif len(words) == 2:
+                ngrams['bigrams'].add(line)
 
-			elif len(words) == 1:
-				ngrams['unigrams'].add(line)
+            elif len(words) == 1:
+                ngrams['unigrams'].add(line)
 
-	# filter out duplicates and redundant keywords
-	searchspace = ' '.join(list(ngrams['trigrams']))
-	for bigram in copy.copy(ngrams['bigrams']):
-		if bigram in searchspace:
-			ngrams['bigrams'].discard(bigram)
+    # filter out duplicates and redundant keywords
+    searchspace = ' '.join(list(ngrams['trigrams']))
+    for bigram in copy.copy(ngrams['bigrams']):
+        if bigram in searchspace:
+            ngrams['bigrams'].discard(bigram)
 
-	searchspace = ' '.join(list(ngrams['trigrams']) + list(ngrams['bigrams']))
-	for unigram in copy.copy(ngrams['unigrams']):
-		if unigram in searchspace:
-			ngrams['unigrams'].discard(unigram)
+    searchspace = ' '.join(list(ngrams['trigrams']) + list(ngrams['bigrams']))
+    for unigram in copy.copy(ngrams['unigrams']):
+        if unigram in searchspace:
+            ngrams['unigrams'].discard(unigram)
 
-	keywords = sorted(list(ngrams['unigrams']) + list(ngrams['bigrams']) + list(ngrams['trigrams']))
+    keywords = sorted(list(ngrams['unigrams']) + list(ngrams['bigrams']) + list(ngrams['trigrams']))
 
-	valid_keywords = []
-	threads = []
+    valid_keywords = []
+    threads = []
 
-	with open(path + '/__ValidatedKeywords.txt', 'w') as f:
-		pass
+    with open(path + '/__ValidatedKeywords.txt', 'w') as f:
+        pass
 
-	for chunk in [keywords[i::num_threads] for i in xrange(num_threads)]:
-		threads.append(ValThread(len(threads), chunk))
-		print('Starting Thread-' + str(threads[-1].tid))
-		threads[-1].start()
+    for chunk in [keywords[i::num_threads] for i in xrange(num_threads)]:
+        threads.append(ValThread(len(threads), chunk))
+        print('Starting Thread-' + str(threads[-1].tid))
+        threads[-1].start()
 
-	for i, thread in enumerate(threads):
-		thread.join()
-		print('Thread-' + str(thread.tid), 'finished')
-		valid_keywords.extend(thread.valid_keywords)
+    for i, thread in enumerate(threads):
+        thread.join()
+        print('Thread-' + str(thread.tid), 'finished')
+        valid_keywords.extend(thread.valid_keywords)
--- Code/resources/proxyfinder.py
+++ Code/resources/proxyfinder.py
@@ -1,52 +1,49 @@
 # Invocation: python3 proxy_finder.py 1000 500 (mines 1000 http and 500 https proxies)
 
 import asyncio
+import json
 import re
 import sys
-import pprint
-import json
 
 from proxybroker import Broker
 
+
 def __get_proxies(protocol, limit):
-	proxy_list = []
+    proxy_list = []
 
-	async def save(proxies):
-	    while True:
-	        proxy = await proxies.get()
-	        if proxy is None: 
-	        	break
-	        match = re.search('([\d]+\.[\d]+\.[\d]+\.[\d]+:[\d]+)', str(proxy))
-	        proxy_list.append(match.group(1))
-	        print('Count:', len(proxy_list))
+    async def save(proxies):
+        while True:
+            proxy = await proxies.get()
+            if proxy is None:
+                break
+            match = re.search('([\d]+\.[\d]+\.[\d]+\.[\d]+:[\d]+)', str(proxy))
+            proxy_list.append(match.group(1))
+            print('Count:', len(proxy_list))
 
-	proxies = asyncio.Queue()
-	broker = Broker(proxies)
-	tasks = asyncio.gather(broker.find(types=[protocol.upper()], limit=limit), save(proxies))
-	loop = asyncio.get_event_loop().run_until_complete(tasks)
+    proxies = asyncio.Queue()
+    broker = Broker(proxies)
+    tasks = asyncio.gather(broker.find(types=[protocol.upper()], limit=limit), save(proxies))
+    loop = asyncio.get_event_loop().run_until_complete(tasks)
 
-	return proxy_list
+    return proxy_list
 
 if __name__ == "__main__":
-	if len(sys.argv) < 3:
-		sys.exit('Not enough arguments')
-		
-	http_proxies = __get_proxies('http', int(sys.argv[1]))
-	https_proxies = __get_proxies('https', int(sys.argv[2]))
+    if len(sys.argv) < 3:
+        sys.exit('Not enough arguments')
 
-	proxies = {}
-	try:
-		with open('proxies.txt', 'r') as f:
-			proxies = json.load(f)
-	except:
-		proxies['http'] = []
-		proxies['https'] = []
+    http_proxies = __get_proxies('http', int(sys.argv[1]))
+    https_proxies = __get_proxies('https', int(sys.argv[2]))
 
-	with open('proxies.txt', 'w') as f:
-		proxies['http'] = list(set(proxies['http'] + http_proxies))
-		proxies['https'] = list(set(proxies['https'] + https_proxies))
+    proxies = {}
+    try:
+        with open('proxies.txt', 'r') as f:
+            proxies = json.load(f)
+    except:
+        proxies['http'] = []
+        proxies['https'] = []
 
-		print(json.dumps(proxies), file=f)
+    with open('proxies.txt', 'w') as f:
+        proxies['http'] = list(set(proxies['http'] + http_proxies))
+        proxies['https'] = list(set(proxies['https'] + https_proxies))
 
-	
-	
+        print(json.dumps(proxies), file=f)
